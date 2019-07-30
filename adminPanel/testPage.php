249e2562d78217d1003cb92b26bf94e8a58bedae
yehyahamzawy <yehya1605639@miuegypt.edu.eg>
2019-07-30 10:37:18 +0200

    .

--- adminPanel/testPage.php
+++ adminPanel/testPage.php
@@ -2,7 +2,7 @@
 //Template
 $title1 = "users";
 $title2 = "view";
-$previous =  array('editor' => "../pages/editor.php", 'checker' => "../pages/checker.php" );
+//$previous =  array('editor' => "../pages/editor.php", 'checker' => "../pages/checker.php" );
 include_once "basicPageHeader.php";
 //----Example:----  viewUsers(); 
 include_once "basicPageFooter.php";
